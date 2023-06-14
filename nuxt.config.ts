8dbc1484ba27140b1ae84e30deb0dd59aba799c1
Søren Madsen <soeren@lait.dk>
2023-06-14 16:13:01 +0200

    Set public runtime.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { createResolver } from '@nuxt/kit'
+import { createResolver } from "@nuxt/kit";
 
-const { resolve } = createResolver(import.meta.url)
+const { resolve } = createResolver(import.meta.url);
 
 export default defineNuxtConfig({
-  css: [resolve('./assets/main.css')],
-  // ssr:false,
-  runtimeConfig:{
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET,
-    BASE_URL: process.env.BASE_URL,
-  }
-})
+	css: [resolve("./assets/main.css")],
+	// ssr:false,
+	runtimeConfig: {
+		public: {
+			CLIENT_ID: process.env.CLIENT_ID,
+			CLIENT_SECRET: process.env.CLIENT_SECRET,
+			BASE_URL: process.env.BASE_URL,
+		},
+	},
+});

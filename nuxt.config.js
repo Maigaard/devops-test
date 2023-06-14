4fd5f014ab62f1ce25518fbe2f9fdb3c92c0b062
Søren Madsen <soeren@lait.dk>
2023-06-14 10:49:18 +0200

    Remove tailwind.

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,9 +12,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    "@nuxtjs/eslint-module",
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    "@nuxtjs/tailwindcss"
+    "@nuxtjs/eslint-module"
   ],
   /*
    ** Nuxt.js modules
